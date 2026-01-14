de4c77db649667adf341592ef3ef05e774dd9ab1
Mohaned Hossam <104258736+MOHANED01@users.noreply.github.com>
2026-01-14 17:05:28 +0200

    Update femtoshell.c

--- 05-femtoshell/femtoshell.c
+++ 05-femtoshell/femtoshell.c
@@ -1,7 +1,46 @@
-// Add the needed includes
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
-int femtoshell_main(int argc, char *argv[]) {
-    // Write your code here
+#define SIZE 30000
+int femtoshell_main(int argc, char *argv[])
+{
+    int status = 0;
+    char buf[SIZE];
+  
+    while(1){
+        
+	    printf("Femto shell prompt > ");
+	    if(fgets(buf,SIZE,stdin)!=0)
+         {
 
+            if(strcmp(buf,"exit\n")==0)
+	    {
+                printf("Good Bye\n");
+                exit(status);
+                break;
+            }
+            else if(strncmp(buf,"echo ",5)==0)
+	    {
+                printf("%s",buf+5);
+            }
+            else if(strcmp(buf,"\n")==0)
+	    {
+	    continue;
+            }
+            else
+	    {
+                
+                printf("Invalid command\n");
+                status = 1 ;
+            }
+        }
+        else
+	{
+            exit(status);
+            break;
+        }
+    }
     return 0;
+
 }
