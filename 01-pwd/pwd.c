8ebafaebbcbc13088fca7740c4e65eb4d718b8db
Mohaned Hossam <104258736+MOHANED01@users.noreply.github.com>
2026-01-14 17:02:31 +0200

    Update pwd.c

--- 01-pwd/pwd.c
+++ 01-pwd/pwd.c
@@ -1,7 +1,35 @@
 // Add the needed includes
+#include <unistd.h>
+#include <limits.h>
+#include <string.h>
 
-int pwd_main() {
-    // Write your code here
-    
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+int pwd_main(int argc,char * argv[])
+{
+  	// Write your code here
+
+char buffer[PATH_MAX];
+
+    if (getcwd(buffer, sizeof(buffer)) == NULL) {
+        return -1;
+    }
+
+    size_t len = strlen(buffer);
+
+    /* Write path */
+    if (write(1, buffer, len) != (ssize_t)len) {
+        return -1;
+    }
+
+    /* Write newline */
+    if (write(1, "\n", 1) != 1) {
+        return -1;
+    }
+
+   
+
     return 0;
 }
