685cada50a6193e277cc99feccac0f686bbe540f
Mohaned Hossam <104258736+MOHANED01@users.noreply.github.com>
2026-01-14 17:03:36 +0200

    Update echo.c

--- 02-echo/echo.c
+++ 02-echo/echo.c
@@ -1,7 +1,34 @@
 // Add the needed includes
+#include <string.h>
+#include <unistd.h>
+
 
 int echo_main(int argc, char *argv[]) {
     // Write your code here
+for(int i=1;i<argc;i++)
+            {
+
+             if(write(1 ,argv[i] ,strlen (argv[i]))<0)
+             {
+               return -1;
+               }
+            
+	     if(i<(argc-1))
+	     {
+		      if(write(1 ," " ,1)<0)
+                         return -1;
+	     }
+	    }
+
+           /*insert enter at the end of line*/
+
+           if(write(1 ,"\n" ,1)<0)
+             {
+               return -1;
+
+             }
+
+
     
     return 0;
 }
